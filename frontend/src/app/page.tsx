edebb0ad30a3700736d11dadcb85a3fab11bec44
Cassius Villareal <cassiusfive@gmail.com>
2025-04-06 02:35:59 -0700

    seal emoji

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,7 +6,10 @@ import { OwnedRepositories } from "@/components/OwnedRepositories";
 export default function Home() {
     return (
         <>
-            <h1 className="text-4xl font-bold mb-8">Flipper Demo</h1>
+            <div className="flex items-center mb-4">
+                <h1 className="text-4xl font-bold mr-4">Flipper Demo</h1>
+                <span className="text-6xl">🦭</span>
+            </div>
             <div className="flex flex-col gap-5">
                 <CreateRepository />
                 <OwnedRepositories />
