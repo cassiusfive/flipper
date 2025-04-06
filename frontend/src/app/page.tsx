5606693f0c566288f6c9fa513913219afd4651cb
Cassius Villareal <cassiusfive@gmail.com>
2025-04-06 02:41:58 -0700

    move title to header

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,15 +5,9 @@ import { OwnedRepositories } from "@/components/OwnedRepositories";
 
 export default function Home() {
     return (
-        <>
-            <div className="flex items-center mb-4">
-                <h1 className="text-4xl font-bold mr-4">Flipper Demo</h1>
-                <span className="text-6xl">🦭</span>
-            </div>
-            <div className="flex flex-col gap-5">
-                <CreateRepository />
-                <OwnedRepositories />
-            </div>
-        </>
+        <div className="flex flex-col gap-5">
+            <CreateRepository />
+            <OwnedRepositories />
+        </div>
     );
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,8 +4,12 @@ import { ConnectButton } from "@mysten/dapp-kit";
 
 export function Header() {
     return (
-        <header className="flex justify-end">
-            <div className="mr-6 my-4">
+        <header className="flex justify-between items-center mx-12">
+            <div className="flex items-center">
+                <h1 className="text-4xl font-bold mr-4">Flipper Demo</h1>
+                <span className="text-6xl">🦭</span>
+            </div>
+            <div className="my-4">
                 <ConnectButton />
             </div>
         </header>
